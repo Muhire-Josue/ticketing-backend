74570eb989829a56ba83fb2d38320c1575acb926
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-08-09 23:42:58 +0200

    add validation signup, auth service

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 const router = express.Router();
-router.get('/api/user/signup', () => {
-
-});
+router.post(
+  "/api/user/signup",
+  [
+    body('email').isEmail().withMessage('Provide a valide email'),
+    body('password')
+      .trim()
+      .isLength({ min: 3, max: 20 })
+      .withMessage('Provide a valid password'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    return res.json({ msg: "Signup" });
+  }
+);
 
 export { router as signupRouter };
