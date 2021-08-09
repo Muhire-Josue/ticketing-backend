63c6f23e769a0846d566dbd91f3adc7baa1a01fb
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-08-09 13:29:14 +0200

    add routes, auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRoute } from './routes/signout';
 
 const app = express();
 app.use(json());
-app.use(currentUserRouter)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRoute);
 
 app.listen(3000, () => console.log('listening on port 3000'));
---
+++ auth/src/routes/signout.ts
@@ -0,0 +1,5 @@
+import express from "express";
+const router = express.Router();
+router.post("/api/user/signout", () => {});
+
+export { router as signoutRoute };
