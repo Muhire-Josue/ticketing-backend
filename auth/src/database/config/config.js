5a1ccaf7ac3b6f60f21c03f673fd06441cb60530
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-09-04 09:21:55 +0200

    configuring ts to work with sequelize, auth service

---
+++ auth/src/database/config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+console.log(`==========>>>>>`, process.env.DATABASE_URL);
+const common = {
+  use_env_variable: process.env.DATABASE_URL,
+  dialect: 'postgres',
+  logging: false,
+  seederStorage: 'sequelize',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 60000,
+    idle: 10000,
+  }
+};
+
+module.exports = {
+  development: {
+    ...common,
+  },
+  test: {
+    ...common,
+    use_env_variable: 'DATABASE_URL_TEST',
+  },
+  staging: {
+    ...common,
+  },
+  production: {
+    ...common,
+  },
+};
--- auth/src/database/models/index.ts
+++ auth/src/database/models/index.ts
@@ -1,19 +1,18 @@
-import "dotenv/config";
-import { Sequelize } from "sequelize";
-import DBConfig from "../config";
+import 'dotenv/config';
+import { Sequelize } from 'sequelize';
+import DBConfig from '../config/config';
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 
-const config = DBConfig[env];
-
+const config: any = DBConfig[env];
 interface Database {
   sequelize: Sequelize;
 }
 
 export const sequelize = new Sequelize(
   process.env[config.use_env_variable] as string,
-  config
-);
+  config,
+)
 
 const db: Database = {
   sequelize,
