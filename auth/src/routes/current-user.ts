537aa3b5f8d5e4118b8ebc270acc44ccbd840cf1
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-10-04 08:33:02 +0200

    add current user, auth service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
-router.get('/api/user/currentuser', () => {
-
+router.get('/api/user/currentuser', (req, res) => {
+  const bearerHeader = req.headers.authorization;
+  if (bearerHeader === undefined) {
+    return res.status(401).send({error: 'Token Undefined'});
+  }
+  const bearer = bearerHeader.split(' ');
+  const bearerToken = bearer[1];
+  return jwt.verify(bearerToken, 'secret', (error, data) => {
+    if (error) {
+      return res.status(402).send({error: 'invalid Token'});
+    }
+    return res.status(200).send({data});
+  });
 });
 
 export { router as currentUserRouter };
