4145d6e67a13c1371a53e949b1f1fd42b1a2cdc6
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-10-22 02:39:24 +0200

    rolling back, auth service

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from "express";
-import authentication from '../middlewares/current-user';
+// import authentication from '../middlewares/current-user';
+// @ts-ignore
+import { authentication } from "@mjmodules/common";
 const router = express.Router();
-router.get("/api/users/logout", authentication, (req: Request, res: Response) => {
-  req.currentUser = undefined;
-  return res.send({});
-});
+router.get(
+  "/api/users/logout",
+  authentication,
+  (req: Request, res: Response) => {
+    req.currentUser = undefined;
+    return res.send({});
+  }
+);
 
 export { router as signoutRouter };
